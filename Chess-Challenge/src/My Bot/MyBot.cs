795bf4e8c232841bc81834f80941d904f5fc83b0
BeefyAndTheDucks <simonsej123gamerminecraft@gmail.com>
2023-07-25 19:31:34 +0200

    Update MyBot.cs

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -12,7 +12,7 @@ public class MyBot : IChessBot
     private readonly int immediateMateScore = 100000;
     private readonly int positiveInfinity = 9999999;
     private readonly int negativeInfinity = -9999999;
-    private readonly int drawScore = 10000000;
+    private readonly int drawScore = 90000;
     private readonly int abortSearchTimeLeftMillis = 20000;
     private readonly int lookForDrawTimeLeftMillis = 10000;
 
@@ -163,100 +163,91 @@ public class MyBot : IChessBot
     {
         var whiteMaterial = CountMaterial(true);
         var blackMaterial = CountMaterial(false);
-        return (whiteMaterial + EvaluatePieceSquareTables(true, EndgamePhaseWeight(whiteMaterial - GetPieceCount(Pawn, true) * 100)) -
-                    (blackMaterial + EvaluatePieceSquareTables(false, EndgamePhaseWeight(blackMaterial - GetPieceCount(Pawn, false) * 100)))) * (board.IsWhiteToMove ? 1 : -1);
+        return 
+            (whiteMaterial + EvaluatePieceSquareTables(true, EndgamePhaseWeight(whiteMaterial - GetPieceCount(Pawn, true) * 100)) -
+            (blackMaterial + EvaluatePieceSquareTables(false, EndgamePhaseWeight(blackMaterial - GetPieceCount(Pawn, false) * 100)))) * (board.IsWhiteToMove ? 1 : -1);
     }
 
-    private float EndgamePhaseWeight(int materialCountWithoutPawns) => Min(1, materialCountWithoutPawns * 0.0003f);
+    private float EndgamePhaseWeight(int materialCountWithoutPawns) => 1 - Min(1, materialCountWithoutPawns * 0.0003f);
 
-    private int EvaluatePieceSquareTables(bool white, float endgamePhaseWeight) => 
-            EvaluatePieceSquareTable(pawnsBonusSqaures, board.GetPieceList(Pawn, white), white) + 
-            EvaluatePieceSquareTable(knightsBonusSqaures, board.GetPieceList(Knight, white), white) +
-            EvaluatePieceSquareTable(bishopsBonusSqaures, board.GetPieceList(Bishop, white), white) +
-            EvaluatePieceSquareTable(rooksBonusSqaures, board.GetPieceList(Rook, white), white) +
-            EvaluatePieceSquareTable(queensBonusSqaures, board.GetPieceList(Queen, white), white) + 
-            (int)(ReadPieceSquareTable(kingBonusSqaures, board.GetKingSquare(white), white) * endgamePhaseWeight);
+    private int EvaluatePieceSquareTables(bool white, float endgamePhaseWeight)
+    {
+        var kingSquare = board.GetKingSquare(white);
+
+        return EvaluatePieceSquareTable(ScoreType.Pawn, board.GetPieceList(Pawn, white), white) +
+            EvaluatePieceSquareTable(ScoreType.Knight, board.GetPieceList(Knight, white), white) +
+            EvaluatePieceSquareTable(ScoreType.Bishop, board.GetPieceList(Bishop, white), white) +
+            EvaluatePieceSquareTable(ScoreType.Rook, board.GetPieceList(Rook, white), white) +
+            EvaluatePieceSquareTable(ScoreType.Queen, board.GetPieceList(Queen, white), white) +
+            GetPieceBonusScore((endgamePhaseWeight >= 0.5f) ? (endgamePhaseWeight >= 0.8f) ? ScoreType.KingHunt : ScoreType.KingEndgame : ScoreType.King, white, kingSquare.Rank, kingSquare.File);
+    } 
     
-    private int EvaluatePieceSquareTable(int[] table, PieceList pieceList, bool isWhite)
+    private int EvaluatePieceSquareTable(ScoreType type, PieceList pieceList, bool white)
     {
         var value = 0;
         foreach (Piece piece in pieceList)
-            value += ReadPieceSquareTable(table, piece.Square, isWhite);
+            value += GetPieceBonusScore(type, white, piece.Square.Rank, piece.Square.File);
 
         return value;
     }
 
-    private int ReadPieceSquareTable(int[] table, Square square, bool isWhite)
-    {
-        if (isWhite)
-            square = new (square.Index ^ 0b111000); // Flip the table upside down if we are white
+    //enumeration to keep track externally of 
+    //which byte is for which scores
+    private enum ScoreType { Pawn, Knight, Bishop, Rook, Queen, King, KingEndgame, KingHunt };
 
-        return table[(square.File > 3 ? square.File ^ 7 : square.File) + square.Index / 8 * 4];
+    //Assuming you put your packed data table into a table called packedScores.
+    private int GetPieceBonusScore(ScoreType type, bool isWhite, int rank, int file)
+    {
+        //Because the arrays are 8x4, we need to mirror across the files.
+        if (file > 3) file = 7 - file;
+        //Additionally, if we're checking black pieces, we need to flip the board vertically.
+        if (!isWhite) rank = 7 - rank;
+        ulong bytemask = 0xFF;
+        //first we shift the mask to select the correct byte              ↓
+        //We then bitwise-and it with PackedScores            ↓
+        //We finally have to "un-shift" the resulting data to properly convert back       ↓
+        //We convert the result to an sbyte, then to an int, to ensure it converts properly.
+        var unpackedData = (int)(sbyte)((packedScores[rank, file] & (bytemask << (int)type)) >> (int)type);
+        //inverting eval scores for black pieces
+        if (!isWhite) unpackedData *= -1;
+        return unpackedData;
     }
 
-    private readonly int[] pawnsBonusSqaures = {
-            0,   0,  0,  0, 
-            50, 50, 50, 50,
-            10, 10, 20, 30,
-            5,   5, 10, 25, 
-            0,   0,  0, 20, 
-            5,  -5,-10,  0, 
-            5,  10, 10,-20,
-            0,   0,  0,  0, 
-    };
-
-    private readonly int[] knightsBonusSqaures = {
-            -50,-40,-30,-30,
-            -40,-20,  0,  0,
-            -30,  0, 10, 15,
-            -30,  5, 15, 20,
-            -30,  0, 15, 20,
-            -30,  5, 10, 15,
-            -40,-20,  0,  5,
-            -50,-40,-30,-30,
-    };
-
-    private readonly int[] bishopsBonusSqaures = {
-            -20,-10,-10,-10,
-            -10,  0,  0,  0,
-            -10,  0,  5, 10,
-            -10,  5,  5, 10,
-            -10,  0, 10, 10,
-            -10, 10, 10, 10,
-            -10,  5,  0,  0,
-            -20,-10,-10,-10,
-    };
-
-    private readonly int[] rooksBonusSqaures = {
-             0,  0,  0,  0, 
-             5, 10, 10, 10, 
-            -5,  0,  0,  0,
-            -5,  0,  0,  0,
-            -5,  0,  0,  0,
-            -5,  0,  0,  0,
-            -5,  0,  0,  0,
-             0,  0,  0,  5, 
-    };
-
-    private static readonly int[] queensBonusSqaures = {
-            -20,-10,-10, -5, 
-            -10,  0,  0,  0, 
-            -10,  0,  5,  5, 
-            -5,   0,  5,  5,  
-             0,   0,  5,  5,
-            -10,  5,  5,  5, 
-            -10,  0,  5,  0, 
-            -20,-10,-10, -5, 
-    };
-
-    private readonly int[] kingBonusSqaures = {
-            -30,-40,-40,-50,
-            -30,-40,-40,-50,
-            -30,-40,-40,-50,
-            -30,-40,-40,-50,
-            -20,-30,-30,-40,
-            -10,-20,-20,-20,
-            20,  20,  0,  0,
-            20,  30, 10,  0,
+    private readonly ulong[,] packedScores =
+    {
+        {0x31CDE1EBFFEBCE00, 0x31D7D7F5FFF5D800, 0x31E1D7F5FFF5E200, 0x31EBCDFAFFF5E200},
+        {0x31E1E1F604F5D80A, 0x13EBD80009FFEC0A, 0x13F5D8000A000014, 0x13FFCE000A00001E},
+        {0x31E1E1F5FAF5E232, 0x13F5D80000000032, 0x0013D80500050A32, 0x001DCE05000A0F32},
+        {0x31E1E1FAFAF5E205, 0x13F5D80000050505, 0x001DD80500050F0A, 0xEC27CE05000A1419},
+        {0x31E1EBFFFAF5E200, 0x13F5E20000000000, 0x001DE205000A0F00, 0xEC27D805000A1414},
+        {0x31E1F5F5FAF5E205, 0x13F5EC05000A04FB, 0x0013EC05000A09F6, 0x001DEC05000A0F00},
+        {0x31E213F5FAF5D805, 0x13E214000004EC0A, 0x140000050000000A, 0x14000000000004EC},
+        {0x31CE13EBFFEBCE00, 0x31E21DF5FFF5D800, 0x31E209F5FFF5E200, 0x31E1FFFB04F5E200},
     };
 }
+
+/*
+int MinMax (Board board, Timer timer, int, depth, int alpha, int beta, bool maximazingPlayer)
+int bestEval = maximizingPlayer ? -2147483647 : 2147483647;
+foreach (Move currentMove in moves)
+{
+
+    board.MakeMove(currentMove);
+
+    int evaluation = MinMax(board, timer, depth - 1, alpha, beta, !maximizingPlayer);
+    board.UndoMove(currentMove);
+    bestEval = maximizingPlayer ? Math.Max(bestEval, evaluation) : Math.Min(bestEval, evaluation);
+    alpha = maximizingPlayer ? Math.Max(alpha, evaluation) : alpha;
+    beta = maximizingPlayer ? beta : Math.Min(beta, evaluation);
+    if (beta <= alpha)
+    {
+        break;
+    }
+}
+
+return bestEval;
+} // this is almost the same code as our current one
+// our current one is just much more optimize
+
+
+*/
